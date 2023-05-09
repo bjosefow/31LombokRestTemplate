e6d541738cbc613af81576115f6b6a0c5d07e95a
Betina Josefowicz <betina.josefowicz@gmail.com>
2023-05-09 15:31:26 +0200

    init

---
+++ src/main/java/com/example/lombokzadaniekoncowe/LombokZadanieKoncoweApplication.java
@@ -0,0 +1,13 @@
+package com.example.lombokzadaniekoncowe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LombokZadanieKoncoweApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LombokZadanieKoncoweApplication.class, args);
+    }
+
+}
